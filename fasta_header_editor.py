d014582d6ff992b786742d28cce5702dff75b734
Chad Smith <chad.smith123@gmail.com>
2016-07-31 13:58:28 -0500

    Added regular expression support to fasta_header_editor.py

--- fasta_header_editor.py
+++ fasta_header_editor.py
@@ -1,6 +1,7 @@
 #!/bin/python
 # Chad Smith 2016-07-12
 import sys
+import re
 import string
 import argparse
 
@@ -18,9 +19,9 @@ def count(args):
                 # Find and replace.
                 if args.pattern is not None:
                         for i in args.pattern:
-	        	    line=line.replace(i,"")
+	        	    line=re.sub(i,"",line)
                 if args.replace is not None:
-                    line=line.replace(args.replace[0],args.replace[1])
+                    line=re.sub(args.replace[0],args.replace[1],line)
                 sys.stdout.write(line)
 	    else:
 	        sys.stdout.write(line)
@@ -28,10 +29,10 @@ def count(args):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Edits header of Genbank FASTA files')
-	parser.add_argument('fasta',type=str)
 	parser.add_argument('--delete', dest='pattern', nargs='+', help='Pattern(s) to delete')
 	parser.add_argument('--replace', nargs=2, help='Replace first argument with the second')
 	parser.add_argument('--genbank', action='store_true',help='Reformats Genbank header if present')
+	parser.add_argument('fasta',type=str)
 	if len(sys.argv)==1:
 		parser.print_help()
 		sys.exit()
