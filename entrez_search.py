b189747b34582109afb201c0a42626189bade607
chadsmith123 <chad.smith123@gmail.com>
2017-08-02 17:05:50 -0500

    Added database option

--- entrez_search.py
+++ entrez_search.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 """
 ENTREZ Search
 Chad Smith 07/11/16
@@ -16,25 +16,26 @@ def count(args):
 	Entrez.email = "chad.smith123@gmail.com"
 	# Count the number of records if --count is given.
 	if args.count is True:
-		handle = Entrez.esearch(db="nucleotide", term=args.pattern,rettype='count')
+		handle = Entrez.esearch(db=args.db, term=args.pattern,rettype='count')
 		records = Entrez.read(handle)
 		print(records)
 		exit(1)	
 
 	# Collect accession numbers
-	handle = Entrez.esearch(db="nucleotide", term=args.pattern,retmax=args.num,sort='relevance')
+	handle = Entrez.esearch(db=args.db, term=args.pattern,retmax=args.num,sort='relevance')
 	records = Entrez.read(handle)
 	accessions=records["IdList"]
 
 	# Fetch entrez gb and write to stdout
 	for i in accessions:
-	    net_handle=Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
+	    net_handle=Entrez.efetch(db=args.db, id=i, rettype="gb", retmode="text")
 	    print(net_handle.read())
 	
 if __name__ == '__main__':
         parser = argparse.ArgumentParser(description='Search genbank and returns flat files')
         parser.add_argument('-pattern', dest='pattern', type=str, help='Search pattern')
         parser.add_argument('-n', dest='num', default='20', type=int, help='Number of records to return')
+        parser.add_argument('-db', dest='db', default='nucleotide', type=str, help='NCBI database')
         parser.add_argument('--count', action='store_true',help='Return number of records and exit')
         if len(sys.argv)==1:
                 parser.print_help()
